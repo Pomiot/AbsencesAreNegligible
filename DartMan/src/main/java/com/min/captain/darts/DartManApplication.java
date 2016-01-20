65c8c7762b124fc0afffad282d4dfedd7fe6715e
Pomiot <grzesiak.t@gmail.com>
2016-01-20 19:50:46 +0100

    DartMan initial commit

---
+++ DartMan/src/main/java/com/min/captain/darts/DartManApplication.java
@@ -0,0 +1,12 @@
+package com.min.captain.darts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DartManApplication.class, args);
+	}
+}
---
+++ DartMan/src/test/java/com/min/captain/darts/DartManApplicationTests.java
@@ -0,0 +1,16 @@
+package com.min.captain.darts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DartManApplication.class)
+public class DartManApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
