3de277c3885ac6d85a28b64c9e363bd1ef853282
Pomiot <pomiot@gmail.com>
2016-01-23 11:10:53 +0100

    add stub methods for rest playerController

--- DartMan/src/main/java/com/min/captain/darts/application/PlayerController.java
+++ DartMan/src/main/java/com/min/captain/darts/application/PlayerController.java
@@ -1,21 +1,17 @@
 package com.min.captain.darts.application;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.min.captain.darts.application.player.Player;
 import com.min.captain.darts.utils.HibernateUtils;
 
 @RestController
-@RequestMapping(value="/player")
+@RequestMapping(value="/players")
 public class PlayerController 
 {
-	@RequestMapping(value="/test")
+	@RequestMapping(method = RequestMethod.POST)
 	public Player createPlayer(@RequestParam(value="first") String firstName, 
-    		@RequestParam(value="last") String lastName) 
-	{
+    		@RequestParam(value="last") String lastName){
         Player player = new Player(firstName, lastName);
         
         HibernateUtils.saveToDb(player);
@@ -24,8 +20,24 @@ public class PlayerController
     }
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public void printSomething()
-	{
-		System.out.println("test");
+	public void getPlayers(){
+		System.out.println("getPlayers method.");
 	}
+
+	@RequestMapping(value = "/{playerId}", method = RequestMethod.GET)
+	public void getPlayerByID(@PathVariable String playerId){
+		System.out.println("getPlayerByID method - id: " + playerId);
+	}
+
+	@RequestMapping(value = "/{playerId}", method = RequestMethod.PUT)
+	public void modifyPlayer(@PathVariable String playerId){
+		System.out.println("modifyPlayer method - id: " + playerId);
+	}
+
+	@RequestMapping(value = "/{playerId}", method = RequestMethod.DELETE)
+	public void deletePlayer(@PathVariable String playerId){
+		System.out.println("deletePlayer method - id: " + playerId);
+	}
+
+
 }
