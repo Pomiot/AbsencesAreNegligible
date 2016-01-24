2af5e2cacf1d33e8c915a34585f90d25ecfd69f1
Pomiot <grzesiak.t@gmail.com>
2016-01-25 00:54:52 +0100

    update match resource api description - even moar

--- DartMan/src/main/java/pl/edu/amu/rest/MatchResource.java
+++ DartMan/src/main/java/pl/edu/amu/rest/MatchResource.java
@@ -1,7 +1,6 @@
 package pl.edu.amu.rest;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import pl.edu.amu.database.DummyMatchRepository;
 import pl.edu.amu.database.MatchRepository;
 import pl.edu.amu.rest.dto.Match;
@@ -29,7 +28,7 @@ public class MatchResource {
 
     @POST
     @ApiOperation(value = "Creates new match", response = Match.class)
-    public Match saveMatch(Match match) {
+    public Match saveMatch(@ApiParam(value = "Match object to save.", required = true) Match match) {
         matchRepository.addMatch(match);
 
         return match;
@@ -38,48 +37,63 @@ public class MatchResource {
     @GET
     @Path("/{matchId}")
     @ApiOperation(value = "Gets match by given id", response = Match.class)
-    public Match getMatch(@PathParam("matchId") String matchId) {
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
+    public Match getMatch(@ApiParam(value = "Id of match to fetch.", required = true) @PathParam("matchId") String matchId) {
         return matchRepository.getMatchById(matchId);
     }
 
     @DELETE
     @Path("/{matchId}")
     @ApiOperation(value = "Deletes match by given id")
-    public void deleteMatch(@PathParam("matchId") String matchId) {
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
+    public void deleteMatch(@ApiParam(value = "Id of match to remove.", required = true) @PathParam("matchId") String matchId) {
         matchRepository.deleteMatch(matchId);
     }
 
     @PUT
     @Path("/{matchId}")
     @ApiOperation(value = "Modifies match with given id")
-    public void modifyMatch(@PathParam("matchId") String matchId, Match match) {
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
+    public void modifyMatch(@ApiParam(value = "Id of match to modify.", required = true) @PathParam("matchId") String matchId, Match match) {
         matchRepository.updateMatchById(matchId, match);
     }
 
     @GET
     @Path("/{matchId}/players")
     @ApiOperation(value = "Gets players that take part in match with given id", response = Player.class, responseContainer = "List")
-    public List<Player> getPlayersInMatch(@PathParam("matchId") String matchId) {
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
+    public List<Player> getPlayersInMatch(@ApiParam(value = "Id of match to fetch players from.", required = true)@PathParam("matchId") String matchId) {
         return matchRepository.getPlayersInMatch(matchId);
     }
 
     @GET
     @Path("/{matchId}/throws")
     @ApiOperation(value = "Gets all throws in match with given id", response = ThrowSet.class, responseContainer = "List")
-    public List<ThrowSet> getThrowsInMatch(@PathParam("matchId") String matchId){
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
+    public List<ThrowSet> getThrowsInMatch(@ApiParam(value = "Id of match to fetch results from.", required = true)@PathParam("matchId") String matchId){
         return matchRepository.getAllThrowsInMatch(matchId);
     }
 
     @GET
     @Path("/{matchId}/round/{roundNumber}")
     @ApiOperation(value = "Gets all throws in given round in match with given id", response = ThrowSet.class, responseContainer = "List")
-    public List<ThrowSet> getThrowsInRound(@PathParam("matchId") String matchId, @PathParam("roundNumber") Integer roundNumber){
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
+    public List<ThrowSet> getThrowsInRound(@ApiParam(value = "Id of match to fetch results from.", required = true)@PathParam("matchId") String matchId,
+                                           @ApiParam(value = "Round number.", required = true) @PathParam("roundNumber") Integer roundNumber){
         return matchRepository.getThrowsInRound(matchId, roundNumber);
     }
 
     @POST
     @Path("/{matchId}/round/{roundNumber}")
     @ApiOperation(value = "Adds throw to given round in match with given id", response = ThrowSet.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Match not found") })
     public ThrowSet addThrowSetToRound(@PathParam("matchId") String matchId, @PathParam("roundNumber") Integer roundNumber, ThrowSet throwSet){
         return matchRepository.addThrowSetToRound(matchId, roundNumber, throwSet);
     }
